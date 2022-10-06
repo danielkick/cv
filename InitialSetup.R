d35783f637dc53d588983ca597753395f21dbaec
danielkick <daniel.r.kick@gmail.com>
2022-10-06 08:24:42 -0500

    Added default template

---
+++ InitialSetup.R
@@ -0,0 +1,23 @@
+# Useful Refs
+# https://annekelincoln.com/resume-in-r/
+# https://shaziaruybal.github.io/automate-cv-rmd/
+
+# for (f in list.files()){
+#   if( f == 'InitialSetup.R'){
+#     print(f)
+#   } else{
+#     file.remove(f)
+#   }
+# }
+
+
+# run ?datadrivencv::use_datadriven_cv to see more details
+# datadrivencv::use_datadriven_cv(
+#   full_name = "Daniel R. Kick",
+#   data_location = 'https://docs.google.com/spreadsheets/d/1RcaTDUGQMWbB2Tt_sWp5iPKhOFhXNpnl737Fgs88cEU',
+#   pdf_location = "https://github.com/danielkick/cv/raw/master/cv.pdf",
+#   html_location = "https://danielkick.com/cv.pdf",
+#   source_location = "https://github.com/danielkick/cv"#,
+#   # use_network_logo = FALSE
+# )
+#
